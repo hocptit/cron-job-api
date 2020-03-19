5076307748bf3c0eafd76809e35a4028798f9c27
hocpit <nguyenthiahocptit@gmail.com>
2020-03-19 11:28:16 +0700

    init project

---
+++ test.js
@@ -0,0 +1,25 @@
+// var date = new Date(2020, 2, 14, 11, 18, 55);
+// var x = 'Tada!';
+// var a = "hello"
+// var j = schedule.scheduleJob(date, function(y, c){
+// 	console.log(y, c);
+// }.bind(null,x, a));
+// x = 'Changing Data';
+
+// let a = "xin chao";
+// let b = " hello"
+// let hello = (c, d)=>{
+// 	console.log(c, d)
+// };
+// console.log(moment().add(10, "s").format("YYYY-MM-DD HH:ss:mm"));
+//
+// let datetime = {
+// 	dayOfWeek: [dayOfWeek.THURSDAY],
+// 	hours: 10,
+// 	minute: 59
+// }
+// console.log(setTimeToDoJobEveryDayOfWeb("123456", datetime, hello, a, b))
+//
+// setInterval(()=>{
+// 	console.log(getListScheduleJob())
+// }, 1000 * 5)
